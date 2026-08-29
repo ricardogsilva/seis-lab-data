32203f6ef851c4cc30290c14031fbeb3ca5d5e4d
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2026-08-29 21:49:19 +0100

    added missing stac conformance class

--- src/seis_lab_data/webapp/stacapi/conformance.py
+++ src/seis_lab_data/webapp/stacapi/conformance.py
@@ -4,6 +4,7 @@ STAC_VERSION: Final[str] = "1.0.0"
 
 CORE: Final[str] = "https://api.stacspec.org/v1.0.0/core"
 ITEM_SEARCH: Final[str] = "https://api.stacspec.org/v1.0.0/item-search"
+COLLECTIONS: Final[str] = "https://api.stacspec.org/v1.0.0/collections"
 OGC_FEATURES: Final[str] = "https://api.stacspec.org/v1.0.0/ogcapi-features"
 OGC_FEATURES_CORE: Final[str] = (
     "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core"
@@ -19,6 +20,7 @@ CHILDREN: Final[str] = "https://api.stacspec.org/v1.0.0-rc.2/children"
 CONFORMS_TO: Final[list[str]] = [
     CORE,
     ITEM_SEARCH,
+    COLLECTIONS,
     OGC_FEATURES,
     OGC_FEATURES_CORE,
     OGC_FEATURES_OAS30,
